05a14517886c6a53fa71195c602fb9754be631fd
nyemb <nyvanyemb@gmail.com>
2022-09-16 16:00:17 -0700

    add new files

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,21 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+	if (a > b)
+	{
+		largest = a;
+	}
+	else
+	{
+		largest = b;
+	}
+	if (largest < c)
 
-    return (largest);
+	{
+		largest = c;
+	}
+
+	return (largest);
 }
