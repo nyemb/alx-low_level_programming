b569546e539368cc1247ac165c41c7a283c44c5a
valjacques Nyemb <nyvanyemb@gmail.com>
2022-10-26 02:37:58 +0100

    change lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef __LISTS__H__
-#define __LISTS__H__
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
  * struct listint_s - singly linked list
@@ -16,6 +16,7 @@
  } listint_t;
 
 
+ int _putchar(char c);
  size_t print_listint(const listint_t *h);
  size_t listint_len(const listint_t *h);
  listint_t *add_nodeint(listint_t **head, const int n);
