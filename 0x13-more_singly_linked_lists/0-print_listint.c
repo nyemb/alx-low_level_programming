0b63949a3ee90d1a015950515ee3b07a0a5f02d2
valjacques Nyemb <nyvanyemb@gmail.com>
2022-10-26 03:01:47 +0100

    fix betty style error

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t size = 0;
 
-		while (h)
-		{
-			size++;
-			printf("%d\n", h->n);
-			h = h->next;
-		}
+	while (h)
+	{
+		size++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 
 
 	return (size);
