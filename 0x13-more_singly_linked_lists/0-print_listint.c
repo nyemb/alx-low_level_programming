37063e37137f44ea842f99d4438b8541cf2ba59a
valjacques Nyemb <nyvanyemb@gmail.com>
2022-10-26 02:58:13 +0100

    modified 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,13 @@ size_t print_listint(const listint_t *h)
 {
 	size_t size = 0;
 
-	if (!(h == NULL))
-	{
-		while (h != NULL)
+		while (h)
 		{
 			size++;
 			printf("%d\n", h->n);
 			h = h->next;
 		}
-	}
+
 
 	return (size);
 }
