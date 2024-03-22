52fbda80137cf5303d233d34c81dca9aa389bf3f
valjacques Nyemb <nyvanyemb@gmail.com>
2024-03-22 02:47:01 +0100

    fix 1_dlistint again

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -11,13 +11,13 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	if (h)
+	if (!h)
 		return (count);
 
 	while (h->prev != NULL)
 		h = h->prev;
 
-	while (!h)
+	while (h)
 	{
 		count++;
 		h = h->next;
